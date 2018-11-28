6b267e7eb32177aa115b29e2cbdf8068bd81cd03
adi <adityabhat@lazaro.in>
2018-11-28 14:11:26 +0530

    Modals : Add hooks on opening and closing then

--- js/modules/modal_box.js
+++ js/modules/modal_box.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
 	// Open Modal Box
 	$('.js_modal_trigger').on('click', function( event ){
+
 		event.preventDefault();
 
 		var modId = $(event.target).data('modId');
@@ -8,10 +9,17 @@ $(document).ready(function() {
 		$('.body').addClass('modal-open'); // Freeze Page Layer
 		$('.js_modal_box_content[data-mod-id="'+ modId +'"]').addClass('active'); // Activate Appropriate Modal Content
 
+		// Trigger the `modal/open` hook with the modal id
+		$( document ).trigger( "modal/open", { id: modId } );
+		$( document ).trigger( "modal/open/" + modId, { id: modId } );
+
 	});
 
 	// close the modal
 	function closeModal () {
+
+		var $activeModal = $( ".js_modal_box_content" ).filter( ".active" );
+		var modalId = $activeModal.data( "modId" );
 
 		if ( $( ".js_modal_modal_box" ).is( ":visible" ) ) {
 			$( ".js_modal_modal_box" ).hide(); // Hide Modal Box
@@ -24,6 +32,10 @@ $(document).ready(function() {
 		}
 
 		$( ".js_modal_close" ).get( 0 ).blur();
+
+		// Trigger the `modal/close` hook with the modal id
+		$( document ).trigger( "modal/close", { id: modalId } );
+		$( document ).trigger( "modal/close/" + modalId, { id: modalId } );
 
 	}
 	// Close Modal Box
